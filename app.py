2146004bc8df3b6f51c2c51f2ebaa8ea15300e9f
Dante-Cadiz <dantecadiz98@mail.com>
2022-11-26 13:16:25 +0000

    Format app.py code to conform to PEP8 standards

--- app.py
+++ app.py
@@ -16,9 +16,10 @@ st.write(
 
 st.write("---")
 
-images_buffer = st.file_uploader('Upload lemon images. You may select more than one.',
-                                        type=['png', 'jpg'],accept_multiple_files=True)
-   
+images_buffer = st.file_uploader(
+                          'Upload lemon images. You may select more than one.',
+                          type=['png', 'jpg'], accept_multiple_files=True)
+
 if images_buffer is not None:
     df_report = pd.DataFrame([])
     for image in images_buffer:
@@ -26,9 +27,11 @@ if images_buffer is not None:
         img_pil = (Image.open(image)).convert('RGB')
         st.info(f"Lemon Image: **{image.name}**")
         img_array = np.array(img_pil)
-        st.image(img_pil, caption=f"Original image - Image Size: {img_array.shape[1]}px width x {img_array.shape[0]}px height")
+        st.image(img_pil, caption=f"Original image - Image Size:"
+                 f"{img_array.shape[1]}px width x"
+                 f" {img_array.shape[0]}px height")
         cleaned_img = clean_input_image(img=img_pil)
         cleaned_array = np.array(cleaned_img)
-        st.image(cleaned_img, caption=f"Cleaned image - Image Size: {cleaned_array.shape[2]}px width x {cleaned_array.shape[1]}px height")
-
-    
+        st.image(cleaned_img, caption=f"Cleaned image - Image Size: "
+                 f"{cleaned_array.shape[2]}px width x "
+                 f"{cleaned_array.shape[1]}px height")
